a18987837e21c04df984772236fc6c42be14eec6
Madhukar <madhukar.reddy326@gmail.com>
2021-04-24 20:39:15 +0530

    added example of parameter lambda expression

--- src/main/java/lambdaExample/Calculator.java
+++ src/main/java/lambdaExample/Calculator.java
@@ -2,13 +2,19 @@ package lambdaExample;
 
 interface calc
 {
-    void show();
+    //void show();//1 comment
+    void sum(int i);
 }
 public class Calculator {
 
     public static void main(String[] args) {
-      calc c =   () -> System.out.println("This method is the implementaion of the show() method");
-      c.show();
+
+    //1 related comment below code
+//      calc c =   () -> System.out.println("This method is the implementaion of the show() method");
+//        c.show();
+        calc c = (i) -> System.out.println("Sum value is : "+i);
+        c.sum(2222);
+
     }
 
 }
