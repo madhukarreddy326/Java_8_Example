f3504f77714d0d2d0f1f4188bffcb63fa7fbc5f0
Madhukar <madhukar.reddy326@gmail.com>
2021-04-25 21:30:15 +0530

    Consumer Example

---
+++ src/main/java/ConsumerPredicateProducerExample/ConsumerExample.java
@@ -0,0 +1,15 @@
+package ConsumerPredicateProducerExample;
+
+import java.util.List;
+
+public class ConsumerExample {
+
+    public static void main(String[] args)
+    {
+//        Consumer<Integer> consumer = t -> System.out.println("Printing : "+t);
+//
+//        consumer.accept(100);
+        List<Integer> list = List.of(10,20,30,40);
+        list.stream().forEach( t -> System.out.println("Printing : "+t));
+    }
+}
